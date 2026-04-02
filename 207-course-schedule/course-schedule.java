ce364fac0d78f7adf1aea00658242cff8043da17
RAMASAI <vtu24986@veltech.edu.in>
2026-04-02 11:19:55 +0530

    Time: 7 ms (60.69%) | Memory: 47.3 MB (26.27%) - LeetSync

---
+++ 207-course-schedule/course-schedule.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[] inDegree = new int[numCourses];
+        List<List<Integer>> adj = new ArrayList<>();
+        
+        for (int i = 0; i < numCourses; i++) {
+            adj.add(new ArrayList<>());
+        }
+        
+        // Build the adjacency list and fill in-degree array
+        for (int[] pre : prerequisites) {
+            int course = pre[0];
+            int prerequisite = pre[1];
+            adj.get(prerequisite).add(course);
+            inDegree[course]++;
+        }
+        
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (inDegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+        
+        int count = 0;
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            count++;
+            
+            for (int neighbor : adj.get(current)) {
+                inDegree[neighbor]--;
+                if (inDegree[neighbor] == 0) {
+                    queue.offer(neighbor);
+                }
+            }
+        }
+        
+        return count == numCourses;
+    }
+}
